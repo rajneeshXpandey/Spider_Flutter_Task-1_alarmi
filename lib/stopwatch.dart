0040ecf14441a8bc7e65fc76dbb5e27ea2b22159
RAJNEESH PANDEY <59232509+RAJNEESH1708@users.noreply.github.com>
2020-08-24 15:49:38 +0530

    Updated StopWatch.dart

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -54,187 +54,181 @@ class _StopwatchState extends State<Stopwatch> {
             children: <Widget>[
 
               /// Display stop watch time
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
-                    child: StreamBuilder<int>(
-                      stream: _stopWatchTimer.rawTime,
-                      initialData: _stopWatchTimer.rawTime.value,
-                      builder: (context, snap) {
-                        final value = snap.data;
-                        final displayTime =
-                        StopWatchTimer.getDisplayTime(value, hours: _isHours);
-                        return Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Text(
-                                displayTime,
-                                style: GoogleFonts.specialElite(
-                                    fontSize: 60.0,
-                                    textStyle: TextStyle(color: Colors.cyanAccent),
-                                    fontWeight: FontWeight.w400),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-
-              /// Lap time.
-              Expanded(
-                flex :2,
-                child: Container(
-                  height: 120,
-                  margin: const EdgeInsets.all(10),
-                  child: StreamBuilder<List<StopWatchRecord>>(
-                    stream: _stopWatchTimer.records,
-                    initialData: _stopWatchTimer.records.value,
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
+                  child: StreamBuilder<int>(
+                    stream: _stopWatchTimer.rawTime,
+                    initialData: _stopWatchTimer.rawTime.value,
                     builder: (context, snap) {
                       final value = snap.data;
-                      if (value.isEmpty) {
-                        return Container();
-                      }
-                      Future.delayed(const Duration(milliseconds: 100), () {
-                        _scrollController.animateTo(
-                            _scrollController.position.maxScrollExtent,
-                            duration: const Duration(milliseconds: 200),
-                            curve: Curves.easeOut);
-                      });
-                      print('Listen records. $value');
-                      return ListView.builder(
-                        controller: _scrollController,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (BuildContext context, int index) {
-                          final data = value[index];
-                          return Column(
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(8),
-                                child: Text(
-                                  '${index + 1})  ${data.displayTime}',
-                                  style:  GoogleFonts.specialElite(
-                          fontSize: 25.0,
-                          textStyle: TextStyle(color: Color(0xff2499B8)),
-                          fontWeight: FontWeight.w300),
-                                ),
-                              ),
-                              const Divider(
-                                height: 1,
-                              )
-                            ],
-                          );
-                        },
-                        itemCount: value.length,
+                      final displayTime =
+                      StopWatchTimer.getDisplayTime(value, hours: _isHours);
+                      return Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              displayTime,
+                              style: GoogleFonts.specialElite(
+                                  fontSize: 60.0,
+                                  textStyle: TextStyle(color: Colors.cyanAccent),
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                        ],
                       );
                     },
                   ),
                 ),
               ),
 
+              /// Lap time.
+              Container(
+                height: 120,
+                margin: const EdgeInsets.all(10),
+                child: StreamBuilder<List<StopWatchRecord>>(
+                  stream: _stopWatchTimer.records,
+                  initialData: _stopWatchTimer.records.value,
+                  builder: (context, snap) {
+                    final value = snap.data;
+                    if (value.isEmpty) {
+                      return Container();
+                    }
+                    Future.delayed(const Duration(milliseconds: 100), () {
+                      _scrollController.animateTo(
+                          _scrollController.position.maxScrollExtent,
+                          duration: const Duration(milliseconds: 200),
+                          curve: Curves.easeOut);
+                    });
+                    print('Listen records. $value');
+                    return ListView.builder(
+                      controller: _scrollController,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (BuildContext context, int index) {
+                        final data = value[index];
+                        return Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                '${index + 1})  ${data.displayTime}',
+                                style:  GoogleFonts.specialElite(
+                        fontSize: 25.0,
+                        textStyle: TextStyle(color: Color(0xff2499B8)),
+                        fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            const Divider(
+                              height: 1,
+                            )
+                          ],
+                        );
+                      },
+                      itemCount: value.length,
+                    );
+                  },
+                ),
+              ),
+
               /// Button
-              Expanded(
-                flex: 2,
-                child: Container(
+              Container(
 
-                  decoration: BoxDecoration(
-                      color: Colors.cyan,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(40.0),
-                          topRight: Radius.circular(40.0))),
-                  child: Padding(
-                   padding: const EdgeInsets.fromLTRB(0,35,0,0),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                child: RaisedButton(
-                                  padding: const EdgeInsets.all(10),
-                                  color: Colors.deepPurpleAccent,
-                                  shape: const StadiumBorder(),
-                                  onPressed: () async {
-                                    _stopWatchTimer.onExecute
-                                        .add(StopWatchExecute.start);
-                                  },
-                                  child: const Text(
-                                    'Start',
-                                    style: TextStyle(fontSize: 25,color: Colors.white),
-                                  ),
+                decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0),
+                        bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0)
+                    )),
+                child: Padding(
+                 padding: const EdgeInsets.fromLTRB(0,35,0,35),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: RaisedButton(
+                                padding: const EdgeInsets.all(10),
+                                color: Colors.deepPurpleAccent,
+                                shape: const StadiumBorder(),
+                                onPressed: () async {
+                                  _stopWatchTimer.onExecute
+                                      .add(StopWatchExecute.start);
+                                },
+                                child: const Text(
+                                  'Start',
+                                  style: TextStyle(fontSize: 25,color: Colors.white),
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                child: RaisedButton(
-                                  padding: const EdgeInsets.all(4),
-                                  color: Colors.amberAccent,
-                                  shape: const StadiumBorder(),
-                                  onPressed: () async {
-                                    _stopWatchTimer.onExecute
-                                        .add(StopWatchExecute.stop);
-                                  },
-                                  child: const Text(
-                                    'Stop',
-                                    style: TextStyle(fontSize: 25,color: Color(0xff533779)),
-                                  ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: RaisedButton(
+                                padding: const EdgeInsets.all(4),
+                                color: Colors.amberAccent,
+                                shape: const StadiumBorder(),
+                                onPressed: () async {
+                                  _stopWatchTimer.onExecute
+                                      .add(StopWatchExecute.stop);
+                                },
+                                child: const Text(
+                                  'Stop',
+                                  style: TextStyle(fontSize: 25,color: Color(0xff533779)),
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                child: RaisedButton(
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: RaisedButton(
 
-                                  padding: const EdgeInsets.all(10),
-                                  color: Colors.tealAccent,
-                                  shape: const StadiumBorder(),
-                                  onPressed: () async {
-                                    _stopWatchTimer.onExecute
-                                        .add(StopWatchExecute.reset);
-                                  },
-                                  child: const Text(
-                                    'Reset',
-                                    style: TextStyle(fontSize: 25,color: Color(0xff533779)),
-                                  ),
+                                padding: const EdgeInsets.all(10),
+                                color: Colors.tealAccent,
+                                shape: const StadiumBorder(),
+                                onPressed: () async {
+                                  _stopWatchTimer.onExecute
+                                      .add(StopWatchExecute.reset);
+                                },
+                                child: const Text(
+                                  'Reset',
+                                  style: TextStyle(fontSize: 25,color: Color(0xff533779)),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(0).copyWith(right: 0),
-                                child: RaisedButton(
-                                  padding: const EdgeInsets.all(4),
-                                  color: Color(0xffFF2982),
-                                  shape: const StadiumBorder(),
-                                  onPressed: () async {
-                                    _stopWatchTimer.onExecute
-                                        .add(StopWatchExecute.lap);
-                                  },
-                                  child: const Text(
-                                    'Lap',
-                                    style: TextStyle(fontSize: 30,color: Colors.white),
-                                  ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(0).copyWith(right: 0),
+                              child: RaisedButton(
+                                padding: const EdgeInsets.all(4),
+                                color: Color(0xffFF2982),
+                                shape: const StadiumBorder(),
+                                onPressed: () async {
+                                  _stopWatchTimer.onExecute
+                                      .add(StopWatchExecute.lap);
+                                },
+                                child: const Text(
+                                  'Lap',
+                                  style: TextStyle(fontSize: 30,color: Colors.white),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               )
